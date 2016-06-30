26e61cbf6897eaa8050846b6b62d283886d7c97c
Kevin Diaz <kevindiazster@gmail.com>
2016-06-30 01:39:20 -0500

    Magic 8-Ball random answer generator

---
+++ 02-data-types/magic-8.js
@@ -0,0 +1,3 @@
+// Magic 8
+var possibleAnswers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it', 'yes, Most likely', 'Outlook good', 'Yes, Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
+console.log(possibleAnswers[Math.floor((Math.random() * 19) + 1)]);
